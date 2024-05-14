ad3530aee72179736ee7c596fdfd5a2fc08df639
Tiago Colli <54643665+tiagoColli@users.noreply.github.com>
2024-05-14 20:14:39 -0300

    Initial commit

---
+++ test/support/factory.ex
@@ -0,0 +1,9 @@
+defmodule ElixirBoilerplate.Factory do
+  @moduledoc false
+  use ExMachina.Ecto, repo: ElixirBoilerplate.Repo
+
+  # This is a sample factory to make sure our setup is working correctly.
+  def name_factory(_) do
+    Faker.Person.name()
+  end
+end
